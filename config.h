bf0d7d26588cce8108a546073454d6573cf40c07
Ernie <Emassei@users.noreply.github.com>
2020-06-03 21:03:38 -0500

    change the delimiter and get rid of the help icon

--- config.h
+++ config.h
@@ -20,11 +20,11 @@ static const Block blocks[] = {
 	{"",	"battery",	5,	3},
 	{"",	"clock",	60,	1},
 	{"",	"internet",	5,	4},
-	{"",	"help-icon",	0,	15},
+        /* {"",	"help-icon",	0,	15}, */
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char *delim = " ";
+static char *delim = " | ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
